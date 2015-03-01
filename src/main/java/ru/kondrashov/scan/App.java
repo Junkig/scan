b7bda6dbe42d457c1de4af67917ea29abbd40067
Junkig <junkig@mail.ru>
2015-03-01 12:36:11 +0400

    init

---
+++ src/main/java/ru/kondrashov/scan/App.java
@@ -0,0 +1,13 @@
+package ru.kondrashov.scan;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
